7ebc127d7ad5c48808eb32a0637083746dc4bd86
Chaerin <chaexxrin@gmail.com>
2024-04-05 13:10:56 +0900

    .

--- app/src/main/java/bitcamp/myapp/App.java
+++ app/src/main/java/bitcamp/myapp/App.java
@@ -23,7 +23,7 @@ public class App {
   public static void main(String[] args) throws Exception {
     System.out.println("과제관리 시스템 서버 실행!");
     SpringApplication.run(App.class, args);
-    
+
   }
 
   @GetMapping("/home")
